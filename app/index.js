c7b949ecf7001f2a9445306fbcf5f7e67862b781
Carl Sutherland <carl@humblesoftware.com>
2020-02-01 15:17:05 -0500

    Responsive 3-column image gallery.

--- app/index.js
+++ app/index.js
@@ -1,16 +1,23 @@
 var $ = require('jquery');
 var markers = require('./posts.json');
 
+// Map:
 var Map = require('./LeafletMap');
 var map = new Map({
   markers: markers
 });
 map.draw();
 
+// Images:
+var $img = $('<div class="images"></div>"');
+$('body').append($img);
 for (var i = 0; i < markers.length; i++) {
-  $('body').append(
-    $('<img>').attr('src', markers[i].image)
-  );
+  var marker = markers[i];
+  $img.append($(
+    '<a href="'+marker.link+'" title="'+marker.title+'" target="_blank">' +
+      '<img src="'+marker.image+'"></img>' +
+    '</a>'
+  ));
 }
 
 
